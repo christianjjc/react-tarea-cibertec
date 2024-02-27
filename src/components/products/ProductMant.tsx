8ba66c38fddf705b0656ec8bff4fd3d78a7a0769
christianjjc <christianjjc@gmail.com>
2024-02-27 14:27:33 -0500

    select agregado para categories

--- src/components/products/ProductMant.tsx
+++ src/components/products/ProductMant.tsx
@@ -16,14 +16,14 @@ export const ProductMant = () => {
   const [product, setProduct] = useState<Product>();
   const { id } = useParams();
 
+  const categories = ["electronics", "jewelery", "men's clothing", "women's clothing"];
+
   const {
     handleSubmit,
     register,
     formState: { isValid },
     setValue,
-  } = useForm<FormInputs>({
-    defaultValues: { ...product },
-  });
+  } = useForm<FormInputs>();
 
   async function mostrarProducto() {
     if (id !== "new") {
@@ -52,11 +52,46 @@ export const ProductMant = () => {
     }
   }, [product, setValue]);
 
+  const onSubmit = async (data: FormInputs) => {
+    console.log(data);
+    /*const { images, ...productToSave } = data;
+    const formData = new FormData(); //*crea un objetoformulario de javascript
+    if (product.id) {
+      formData.append("id", product.id ?? "");
+    }
+    formData.append("title", productToSave.title ?? "");
+    formData.append("slug", productToSave.slug ?? "");
+    formData.append("description", productToSave.description ?? "");
+    formData.append("price", productToSave.price.toString() ?? "");
+    formData.append("inStock", productToSave.inStock.toString() ?? "");
+    formData.append("sizes", productToSave.sizes.toString() ?? "");
+    formData.append("tags", productToSave.tags);
+    formData.append("categoryId", productToSave.categoryId);
+    formData.append("gender", productToSave.gender);
+
+    if (images) {
+      for (let i = 0; i < images.length; i++) {
+        formData.append("images", images[i]);
+      }
+    }
+
+    console.log({ formData });
+    const { ok, product: updatedProduct } = await createUpdateProduct(formData);
+    console.log(ok);
+
+    if (!ok) {
+      alert("El producto no se pudo actualizar");
+      return;
+    }
+
+    router.replace(`/admin/product/${updatedProduct?.slug}`); */
+  };
+
   return (
     <>
       <div className="row">
         <div className="col">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="row">
               <div>Producto id:{id}</div>
               <div className="d-flex flex-column  my-2">
@@ -69,7 +104,6 @@ export const ProductMant = () => {
                   {...register("title", { required: true })}
                 />
               </div>
-
               <div className="d-flex flex-column  my-2">
                 <span>price</span>
                 <input
@@ -80,7 +114,6 @@ export const ProductMant = () => {
                   {...register("price", { required: true })}
                 />
               </div>
-
               <div className="d-flex flex-column  my-2">
                 <span>description</span>
                 <textarea
@@ -92,20 +125,28 @@ export const ProductMant = () => {
               </div>
               <div className="d-flex flex-column  my-2">
                 <span>category</span>
-                <input
+                {/* <input
                   type="text"
                   className="p-2 border rounded-md bg-gray-200"
                   id="category"
                   //defaultValue={product?.category}
                   {...register("category", { required: true })}
-                />
+                /> */}
+                <select className="form-select" {...register("category", { required: true })}>
+                  <option value="">[Seleccione]</option>
+                  {categories.map((c) => (
+                    <option key={c} value={c}>
+                      {c}
+                    </option>
+                  ))}
+                </select>
               </div>
               <div className="d-flex flex-column  my-2">
-                <span>category</span>
+                <span>Image</span>
                 <input
                   type="text"
                   className="p-2 border rounded-md bg-gray-200"
-                  id="category"
+                  id="image"
                   //defaultValue={product?.image}
                   {...register("image", { required: true })}
                 />
